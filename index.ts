91b76ef2a1633542cd15431e1d96821b0e66c40e
tranduong18 <147843344+tranduong18@users.noreply.github.com>
2024-09-12 15:13:45 +0700

    9. Cài đặt CORS

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express, { Express } from "express";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
+import cors from "cors";
 dotenv.config();
 
 import { connectDatabase } from "./config/database";
@@ -10,6 +11,18 @@ connectDatabase();
 
 const app: Express = express();
 const port: number | string = process.env.PORT || 3000;
+
+// CORS
+// Cách 1: Tất cả tên miền được phép truy cập
+app.use(cors());
+
+// Cách 2: Áp dụng cho 1 tên miền cụ thể
+// const corsOptions = {
+//   origin: 'https://abc.com',
+//   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// }
+// app.use(cors(corsOptions));
+// End CORS
 
 // parse application/json
 app.use(bodyParser.json());
