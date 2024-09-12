7d90f90a714537c386957c76e41692d2499f7761
tranduong18 <147843344+tranduong18@users.noreply.github.com>
2024-09-12 15:30:15 +0700

    12. Tính năng đăng nhập

--- controllers/client/user.controller.ts
+++ controllers/client/user.controller.ts
@@ -7,39 +7,72 @@ import { generateRandomString } from "../../helpers/generate.helper";
 // [POST] /users/register
 export const register = async (req: Request, res: Response) => {
     try {
-      const existUser = await User.findOne({
-        email: req.body.email,
-        deleted: false
-      });
-  
-      if(existUser) {
-        res.json({
-          code: 400,
-          message: "Email đã tồn tại!"
+        const existUser = await User.findOne({
+            email: req.body.email,
+            deleted: false
         });
-        return;
-      }
-  
-      const token = generateRandomString(30);
-  
-      const dataUser = {
-        fullName: req.body.fullName,
-        email: req.body.email,
-        password: md5(req.body.password),
-        token: token,
-      };
-  
-      const user = new User(dataUser);
-      await user.save();
-  
-      res.json({
-        code: 200,
-        message: "Đăng ký thành công!",
-        token: token
-      });
+
+        if (existUser) {
+            res.json({
+                code: 400,
+                message: "Email đã tồn tại!"
+            });
+            return;
+        }
+
+        const token = generateRandomString(30);
+
+        const dataUser = {
+            fullName: req.body.fullName,
+            email: req.body.email,
+            password: md5(req.body.password),
+            token: token,
+        };
+
+        const user = new User(dataUser);
+        await user.save();
+
+        res.json({
+            code: 200,
+            message: "Đăng ký thành công!",
+            token: token
+        });
     } catch (error) {
-      res.json({
-        message: "Not Found"
-      });
+        res.json({
+            message: "Not Found"
+        });
     }
-  }
+}
+
+// [POST] /users/login
+export const login = async (req: Request, res: Response) => {
+    const email: string = req.body.email;
+    const password: string = req.body.password;
+  
+    const user = await User.findOne({
+      email: email,
+      deleted: false
+    });
+  
+    if(!user) {
+      res.json({
+        code: 400,
+        message: "Email không tồn tại!"
+      });
+      return;
+    }
+  
+    if(md5(password) != user.password) {
+      res.json({
+        code: 400,
+        message: "Sai mật khẩu!"
+      });
+      return;
+    }
+  
+    res.json({
+      code: 200,
+      message: "Đăng nhập thành công!",
+      token: user.token
+    });
+}
